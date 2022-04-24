fc306d36f97f51f77567aaf41d3a444f9ad7081a
xwalley <101486355+xwalley@users.noreply.github.com>
2022-04-24 17:09:41 -0400

    brandnav

---
+++ brandnav/menu.js
@@ -0,0 +1,11 @@
+function showmenu(){
+
+  document.getElementById('menuoverlay').style.display="block";
+
+}
+
+function hidemenu(){
+
+  document.getElementById('menuoverlay').style.display="none";
+
+}
