551000afa99585c53d7106de4f0b76d4ea337870
xwalley <101486355+xwalley@users.noreply.github.com>
2022-05-08 22:37:17 -0400

    5Words

---
+++ top5-final/words.js
@@ -0,0 +1,34 @@
+function showWord1(){
+
+    document.getElementById("word1").style.display="block";
+
+    document.getElementById("unavailable1").style.display="none";
+  }
+
+  function showWord2(){
+
+      document.getElementById("word2").style.display="block";
+
+      document.getElementById("unavailable2").style.display="none";
+    }
+
+    function showWord3(){
+
+        document.getElementById("word3").style.display="block";
+
+        document.getElementById("unavailable3").style.display="none";
+      }
+
+      function showWord4(){
+
+          document.getElementById("word4").style.display="block";
+
+          document.getElementById("unavailable4").style.display="none";
+        }
+
+        function showWord5(){
+
+            document.getElementById("word5").style.display="block";
+
+            document.getElementById("unavailable5").style.display="none";
+          }
